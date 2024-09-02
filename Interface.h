306e3e154ec2ce6d639685bdd71d4dc7588e5d05
Abubakar Saeed <abubakarsaeed915@gmail.com>
2024-09-02 20:41:30 +0500

    initial commit

---
+++ Interface.h
@@ -0,0 +1,35 @@
+#ifndef Interface_H
+#define Interface_H
+
+#include "User.h"
+#include "Student.h"
+#include "Staff.h"
+#include "SubjectQuestions.h"
+#include "Quiz.h"
+#include "QuizResultHandler.h"
+
+class Interface {
+	
+    int choice;
+    SubjectQuestions admin;
+    Staff staff;
+    Student student;
+    Quiz quiz;
+    QuizResultHandler result;
+
+public:
+	
+    void welcome();
+    void menu();
+    void staffloginMenu();
+    void studentloginMenu();
+    void subjectManagmentMenu();
+    void quizManagmentMenu();
+    void studentMenu();
+    void staffMenu();
+    void reportMenu();
+    void setColor(int color);
+};
+
+#endif
+
---
+++ main.cpp
@@ -0,0 +1,11 @@
+#include "Interface.h"
+
+int main() {
+	
+	
+    Interface interface;
+    interface.welcome();
+    interface.menu();
+
+    return 0;
+}
